cf9edc4feff4c058880b972c0428547ca95f70cb
Kamil Obst <obst.kamil@gmail.com>
2015-10-13 22:45:15 +0200

    Controller and Model Bugfixes on Students and Teachers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  root 'visitors#index'
+
   devise_for :users
 
   resources :students do
     get :subjects
   end
+
+  resources :teachers do
+  	get :subjects
+  end
+
+  get 'report/subjects', to: 'reports#subjects', as: 'report_subjects'
 end
